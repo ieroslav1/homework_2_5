edb6bcf0ae819ad9acecd32f4c9a96e83fdbe440
ieroslav1 <yaraboom@rambler.ru>
2023-04-17 16:45:27 +0300

    Выполненное задание

---
+++ src/main/java/pro/sky/homework25/service/EmployeeServiceImpl.java
@@ -0,0 +1,56 @@
+package pro.sky.homework25.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.homework25.exception.EmployeeAlreadyAddedException;
+import pro.sky.homework25.exception.EmployeeNotFoundException;
+import pro.sky.homework25.model.Employee;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final List<Employee> employeeList;
+
+    public EmployeeServiceImpl(List<Employee> employeeList) {
+        this.employeeList = new ArrayList<>();
+    }
+
+    @Override
+    public Employee add(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeList.contains(employee)) {
+            throw new EmployeeAlreadyAddedException();
+        }
+        employeeList.add(employee);
+        return employee;
+    }
+
+    @Override
+    public Employee remove(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeList.contains(employee)) {
+            employeeList.remove(employee);
+            return employee;
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    @Override
+    public Employee find(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+
+        if (employeeList.contains(employee)) {
+            return employee;
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    @Override
+    public Collection<Employee> findAll() {
+        return Collections.unmodifiableList(employeeList);
+    }
+}
